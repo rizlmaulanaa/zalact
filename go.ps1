d9a160297475da37dc57356c89a232990027b441
rizlmaulanaa <109032002+rizlmaulanaa@users.noreply.github.com>
2025-08-11 11:27:32 +0800

    Update go.ps1
    Hidden Password

--- go.ps1
+++ go.ps1
@@ -1,8 +1,11 @@
-Write-Host "Hey Zal, How are yo"
-$pw = Read-Host "Prove Youre Zal"
+Write-Host "Hai Zal"
+$securePw = Read-Host "Berikan passwordmu" -AsSecureString
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePw)
+$pw = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
+# Ganti '12345' dengan password kamu
 if ($pw -eq "Itsz") {
     irm https://get.activated.win | iex
 } else {
-    Write-Host "NO YOU ARE NOTT!!! WRONG PASSWORD."
+    Write-Host "Password salah."
 }
