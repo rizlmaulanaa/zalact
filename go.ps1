43984c08296c531981fb16dff4ef3d5ee6237689
rizlmaulanaa <109032002+rizlmaulanaa@users.noreply.github.com>
2025-08-11 12:03:06 +0800

    Update go.ps1

--- go.ps1
+++ go.ps1
@@ -1,11 +1,14 @@
-Write-Host "Hai Zal"
-$securePw = Read-Host "Berikan passwordmu" -AsSecureString
+Write-Host "Heyy Zal"
+$securePw = Read-Host "Give me your authentication" -AsSecureString
 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePw)
 $pw = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-# Ganti '12345' dengan password kamu
-if ($pw -eq "Itsz") {
+# obfuscate Base64
+$encodedPw = "SXRzeg=="
+$decodedPw = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($encodedPw))
+
+if ($pw -eq $decodedPw) {
     irm https://get.activated.win | iex
 } else {
-    Write-Host "Password salah."
+    Write-Host "NO YOU ARE NOT ZAL"
 }
