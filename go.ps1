bc272a1e59062cb8c9ec90e4ad1eb8b44ee41db3
rizlmaulanaa <109032002+rizlmaulanaa@users.noreply.github.com>
2025-08-11 17:10:22 +0800

    Update go.ps1
    Obfuscate

--- go.ps1
+++ go.ps1
@@ -1,14 +1,20 @@
 Write-Host "Heyy Zal"
-$securePw = Read-Host "Give me your authentication" -AsSecureString
-$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePw)
-$pw = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
-# obfuscate Base64
-$encodedPw = "SXRzeg=="
-$decodedPw = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($encodedPw))
+# get the password (secure)
+$a1 = Read-Host "Give me your authentication" -AsSecureString
+$a2 = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($a1)
+$a3 = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($a2)
 
-if ($pw -eq $decodedPw) {
-    irm https://get.activated.win | iex
+# # obfuscate Base64 (exp 12345 -> MTIzNDU=)
+$p1 = "SXRzeg=="
+$p2 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($p1))
+
+# URL Base64
+$u1 = "aHR0cHM6Ly9nZXQuYWN0aXZhdGVkLndpbg=="
+$u2 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($u1))
+
+if ($a3 -eq $p2) {
+    iex (irm $u2)
 } else {
     Write-Host "NO YOU ARE NOT ZAL"
 }
