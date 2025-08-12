d7d079d5541120569f49fc9557bd6a3cec796071
rizlmaulanaa <109032002+rizlmaulanaa@users.noreply.github.com>
2025-08-12 11:51:04 +0800

    Update go.ps1
    url update to script.ps1

--- go.ps1
+++ go.ps1
@@ -11,7 +11,7 @@ $p1 = "RGlueWFsYWlubm4=" #here
 $p2 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($p1))
 
 # main URL Base64
-$u1 = "aHR0cHM6Ly9nZXQuYWN0aXZhdGVkLndpbg=="
+$u1 = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3JpemxtYXVsYW5hYS96YWxhY3QvcmVmcy9oZWFkcy9tYWluL3NjcmlwdC5wczE=" #"aHR0cHM6Ly9nZXQuYWN0aXZhdGVkLndpbg==" 
 $u2 = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($u1))
 
 # pw checkers
